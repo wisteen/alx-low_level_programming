338e43c9f8ef4a12b6ee2c5b486019464fcbc6dc
wisteen <wisdomisaac168@gmail.com>
2022-09-09 00:46:33 +0100

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - this is a function that prints
+ *
+ * Return: 0 mean completed
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
+
