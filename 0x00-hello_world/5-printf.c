cbfe5ee1d4af5765309e65abf9d57cca975d1a26
wisteen <wisdomisaac168@gmail.com>
2022-09-08 23:49:19 +0100

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - A function that prints stuff
+ * Return: Prove that it is successful
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
