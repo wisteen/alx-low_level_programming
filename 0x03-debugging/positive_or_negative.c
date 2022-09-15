1dad868f7a6ec5e1c5283f5538f7af52809ec393
wisteen <wisdomisaac168@gmail.com>
2022-09-15 21:03:05 +0100

    Just succeded

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,12 +6,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	
 
 	if (n > 0)
 		printf("%d is positive\n", n);
