a802daa871eeddd858b0c803399a4658aade1a06
wisteen <wisdomisaac168@gmail.com>
2022-10-26 02:39:36 +0100

    wisteen technology

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,52 +1,32 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * find_listint_loop - function that finds the loop in a linked list.
- * @head: pointer to the head of linked list.
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: pointer to the beginning of the list
  *
- * You are not allowed to use malloc, free or arrays.
- * You can only declare a maximum of two variables in your function.
- *
- * Return: The address of the node where the loop starts, or NULL if,
- * there is no loop.
+ * Return: address of the node where the loop starts or NULL if there's no loop
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
-	/* Use of Floyd’s Cycle-Finding Algorithm */
-	/* let slow_ptr and fast_ptr be two node pointers pointing to the head node */
-	listint_t *slow_ptr = head, *fast_ptr = head;
-	/* if list is empty or has only one node without a loop return NULL */
-	if (head == NULL || head->next == NULL)
-		return (NULL);
-	/* in every iteration slow_ptr ptr moves ahead by one node */
-	slow_ptr = slow_ptr->next;
-	/* in every iteration fast_ptr ptr moves ahead by two nodes */
-	fast_ptr = fast_ptr->next->next;
-	/* search for a loop using slow_ptr and fast_ptr pointers */
-	while (fast_ptr && fast_ptr->next)
+	listint_t *tortoise, *hare;
+
+	tortoise = hare = head;
+	while (tortoise && hare && hare->next)
 	{
-		/**
-		 * slow_ptr and fast_ptr eventually meet at the same node, thus,
-		 * indicating that the linked list contains a loop.
-		 */
-		if (slow_ptr == fast_ptr)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
 			break;
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
+		}
 	}
-	/* if loop does not exists return NULL*/
-	if (slow_ptr != fast_ptr)
+	if (!tortoise || !hare || !hare->next)
 		return (NULL);
-	/* if loop exists, start slow_ptr from head and fast_ptr from meet point */
-	slow_ptr = head;
-	while (slow_ptr != fast_ptr)
+	while (tortoise != hare)
 	{
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next;
+		tortoise = tortoise->next;
+		hare = hare->next;
 	}
-	/* return slow_ptr */
-	return (slow_ptr);
+	return (hare);
 }
